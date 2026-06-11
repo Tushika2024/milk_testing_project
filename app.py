0503ab90c3d50ecdaf9293e0a10b5d95db202f50
Tushika Garg <tushikacoderbsr@gmail.com>
2026-06-11 22:37:23 +0530

    inital_work_ui

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Complaint Analytics",
+    page_icon="📊",
+    layout="wide"
+)
+st.title()
---
+++ utils/loader.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import  numpy as np
+import streamlit as st
+
+@st.cache_data
+def load_data(filepath):
+    df=pd.read_excel(filepath)
+    
+    date_cols=["LogedDate","resolveDate"]
+    for col in date_cols:
+        if col in df.columns:
+            df[col]=pd.to_datetime(df[col],errors="coerce") #value that cannot be converted to the target data type, don't crash.force it to become a missing value (NaN)
+    return df
